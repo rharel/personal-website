057a0807ff5bdcafec7fd2a0f732689743454610
Raoul Harel <raoul.harel@gmail.com>
2022-06-08 23:29:42 +0200

    add pacman webgl build

--- build.sh
+++ build.sh
@@ -9,7 +9,7 @@ mv production/CNAME production/.nojekyll build_artifacts
 rm -rf templates_compiled/* production/*
 npx eleventy --input=templates --output=templates_compiled
 npx parcel build --no-cache --dist-dir production
-cp webroot/* production
+cp -r webroot/* production
 
 # Restore deployment independent files from temporary directory
 mv build_artifacts/CNAME build_artifacts/.nojekyll production
